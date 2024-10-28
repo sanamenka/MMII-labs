4ac828c15806b9d1ab8985902a700c66e46407ec
sanamenka <vega_gera@mail.ru>
2024-10-28 12:20:44 +0700

    второе задание

--- lab1.R
+++ lab1.R
@@ -8,11 +8,25 @@ AR=function(n,tetta) {
   }
   return(x)
 }
-AR1=AR(400,0.5)
-AR2=AR(600,1)
-AR3=AR(700,1.3)
+n=400
+AR1=AR(n,0.7)
+AR2=AR(n,1)
+AR3=AR(n,1.3)
 plot(AR1)
 plot(AR2)
 plot(AR3)
+
+MNK=(sum(AR1[2:n] * AR1[1:(n-1)])/sum(AR1[1:(n-1)]^2))
+
+MP_function = function(tetta) {
+  sum((AR1[2:n] - tetta * AR1[1:(n-1)])^2)
+}
+
+optimal_tetta = optimize(MP_function, interval=c(-100, 100))$minimum
+
+# Вывод результатов
+cat("Оценка МНК:", MNK, "\n")
+cat("Оптимальное значение тетта по МП:", optimal_tetta, "\n")
+
 
 rm(list = ls()); graphics.off()
